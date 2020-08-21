acd05fa411c0ba5ce5ec54c25451c27997fbbc6f
Biswapratik Maharana <biswapratik@Biswapratiks-Mac-mini.local>
2020-08-21 14:54:11 +0530

    Initial Commit

---
+++ WebServices/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WebServices
+//
+//  Created by Biswapratik Maharana on 21/08/20.
+//  Copyright © 2020 Biswapratik Maharana. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
